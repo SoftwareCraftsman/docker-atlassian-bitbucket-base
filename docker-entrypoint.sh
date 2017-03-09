3e0d502bc9c2a4e1916053bc18fd051637a8cc4d
Martin Ahrer <this@martinahrer.at>
2017-03-09 10:17:17 +0100

    Fixes #5 - Upgrade to bitbucket 4.14

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+set -x
 
 # Configure basic configuration parameters
 # see https://confluence.atlassian.com/bitbucketserver/automated-setup-for-bitbucket-server-776640098.html
-# TODO print a warning when any of the credentials have assumed default values
 
-if [ -f bitbucket.properties ]; then
-  if [ ! -f $BITBUCKET_HOME/shared/bitbucket.properties ]; then
+if [ -f ${BITBUCKET_HOME}/bitbucket.properties.template ]; then
+  if [ ! -f ${BITBUCKET_HOME}/shared/bitbucket.properties ]; then
+    mv ${BITBUCKET_HOME}/bitbucket.properties.template ${BITBUCKET_HOME}/shared/bitbucket.properties
+    pushd ${BITBUCKET_HOME}/shared
+
+    if [ -z ${BITBUCKET_DISPLAYNAME+x} ]; then
+      echo "BITBUCKET_DISPLAYNAME not set";
+    else
+      echo "setup.displayName=$BITBUCKET_DISPLAYNAME" >> bitbucket.properties;
+    fi
+
+    if [ -z ${BITBUCKET_BASEURL+x} ]; then
+      echo "BITBUCKET_BASEURL not set";
+    else
+      echo "setup.baseUrl=$BITBUCKET_BASEURL" >> bitbucket.properties;
+    fi
+
     if [ -z ${BITBUCKET_LICENSE+x} ]; then
       echo "BITBUCKET_LICENSE not set";
     else
@@ -24,6 +39,18 @@ if [ -f bitbucket.properties ]; then
       echo "setup.sysadmin.password=${BITBUCKET_PASSWORD:-bitbucket}" >> bitbucket.properties
     fi
 
+    if [ -z ${BITBUCKET_USER_DISPLAYNAME+x} ]; then
+      echo "BITBUCKET_USER_DISPLAYNAME not set";
+    else
+      echo "setup.sysadmin.displayName=${BITBUCKET_USER_DISPLAYNAME:-Bitbucket}" >> bitbucket.properties
+    fi
+
+    if [ -z ${BITBUCKET_EMAIL+x} ]; then
+      echo "BITBUCKET_EMAIL not set";
+    else
+      echo "setup.sysadmin.emailAddress=${BITBUCKET_EMAIL:-bitbucket@server.com}" >> bitbucket.properties
+    fi
+
     if [ -z ${BITBUCKET_JDBC_USER+x} ]; then
       echo "BITBUCKET_JDBC_USER not set";
     else
@@ -35,10 +62,23 @@ if [ -f bitbucket.properties ]; then
     else
       echo "jdbc.password=${BITBUCKET_JDBC_PASSWORD:-bitbucket}" >> bitbucket.properties
     fi
-
-    mv bitbucket.properties $BITBUCKET_HOME/shared/
+    popd
   fi
 fi
 
-# Start bitbucket in foreground mode
-exec $BITBUCKET_INSTALL_DIR/$BITBUCKET_VERSION/bin/start-bitbucket.sh -fg
+#
+# For the startup and permission downgrade credits go to https://bitbucket.org/atlassian/docker-atlassian-bitbucket-server
+#
+
+exec $BITBUCKET_INSTALL_DIR/bin/start-bitbucket.sh "$@"
+
+#if [ "$UID" -eq 0 ]; then
+#    echo "User is currently root. Will change directories to daemon control, then downgrade permission to daemon"
+#    mkdir -p ${BITBUCKET_HOME}/lib &&
+#        chmod -R 700 "${BITBUCKET_HOME}" &&
+#        chown -R ${RUN_USER}:${RUN_GROUP} "${BITBUCKET_HOME}"
+#    # Now drop privileges
+#    exec su -s /bin/bash ${RUN_USER} -c "$BITBUCKET_INSTALL_DIR/bin/start-bitbucket.sh $@"
+#else
+#    exec $BITBUCKET_INSTALL_DIR/bin/start-bitbucket.sh "$@"
+#fi
