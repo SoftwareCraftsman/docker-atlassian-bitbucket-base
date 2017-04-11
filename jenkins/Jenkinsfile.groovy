b7e34e37c6a31a9dae5b9710838397b55e3a95cf
Martin Ahrer <this@martinahrer.at>
2017-04-11 09:42:50 +0200

    Fixes #12 - Prepare for shared library use

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -1,26 +1,26 @@
-def buildImage(def options, def imageName) {
-    sh "docker build ${options} -t ${imageName} ."
+def buildImage(String options, String image) {
+    sh "docker build ${options} -t ${image} ."
 }
 
-def tagImage(def source, def target) {
-    sh(script: "docker tag ${source} ${target}")
+def tagImage(String sourceImage, String targetImage) {
+    sh(script: "docker tag ${sourceImage} ${targetImage}")
 }
 
-def pushImage(def image) {
+def pushImage(String image) {
     sh "docker push ${image}"
 }
 
-def untagImage(def imageName) {
-    sh "docker image rm ${imageName}"
+def untagImage(String image) {
+    sh "docker image rm ${image}"
 }
 
 /**
  * Run an action that requires a Docker registry authentication.
- * @param registryCredentialsId the credentials for the registry authentication will be pulled from the Jenkins credentials store using this credentials id.
+ * @param credentialsId the credentials for the registry authentication will be pulled from the Jenkins credentials store using this credentials id.
  * @param action the registry action to run authenticated.
  */
-def withDockerRegistry(def registryCredentialsId, Closure action) {
-    def dockerCredentials = usernamePassword(credentialsId: registryCredentialsId, passwordVariable: 'DOCKER_REGISTRY_PASSWORD', usernameVariable: 'DOCKER_REGISTRY_USER')
+def withDockerRegistry(String credentialsId, Closure action) {
+    def dockerCredentials = usernamePassword(credentialsId: credentialsId, passwordVariable: 'DOCKER_REGISTRY_PASSWORD', usernameVariable: 'DOCKER_REGISTRY_USER')
     withCredentials([dockerCredentials]) {
         def registry = env.DOCKER_REGISTRY
         sh 'docker login --username=${DOCKER_REGISTRY_USER} --password=${DOCKER_REGISTRY_PASSWORD} ' + registry
@@ -35,15 +35,15 @@ def withDockerRegistry(def registryCredentialsId, Closure action) {
 
 /**
  * This creates a release tag in the git repository and passes the tag into the closure passed as action parameter.
- * @param projectDir
- * @param credentialsId the credentials for the Git reposiutory authentication will be pulled from the Jenkins credentials store using this credentials id.
+ * @param credentialsId the credentials for the Git repository authentication will be pulled from the Jenkins credentials store using this credentials id.
+ * @param workspace the workspace directory is the directory where sources are checked out. Defaults to {@code env.JENKINS_AGENT_WORKSPACE/env.JOB_NAME }
  * @param action the registry action to run with a release tag.
  */
-def withReleaseTag(def projectDir, def credentialsId, Closure action) {
+def withReleaseTag(String credentialsId, String workspace="${env.JENKINS_AGENT_WORKSPACE}/${env.JOB_NAME}", Closure action) {
     def credentials = usernamePassword(credentialsId: credentialsId, usernameVariable: 'GRGIT_USER', passwordVariable: 'GRGIT_PASS')
     withCredentials([credentials]) {
 
-        def dockerRunOptions = "--rm -v ${projectDir}:/project -v dot_gradle:/root/.gradle --workdir=/project"
+        def dockerRunOptions = "--rm -v ${workspace}:/workspace -v dot_gradle:/root/.gradle --workdir=/workspace"
         if (env.GRADLE_OPTS) {
             dockerRunOptions += " -e GRADLE_OPTS=\"${env.GRADLE_OPTS}\""
         }
@@ -59,10 +59,9 @@ def withReleaseTag(def projectDir, def credentialsId, Closure action) {
 }
 
 node('docker') {
-    def isRelease = (params.releaseScope) || (params.releaseStage)
-    def projectDir = "${env.JENKINS_AGENT_WORKSPACE}/${env.JOB_NAME}"
+    def isDeliver = (params.releaseScope) || (params.releaseStage)
     def projectName = "${env.JOB_NAME}_${env.BUILD_NUMBER}"
-    def projectSettings = ["PROJECT_DIR=/${projectDir}", "COMPOSE_PROJECT_NAME=${projectName}"]
+    def projectSettings = ["COMPOSE_PROJECT_NAME=${projectName}"]
     def name = "atlassian-bitbucket-base"
     def buildTag = env.BUILD_NUMBER
     def buildImageName = "${name}:${buildTag}"
@@ -85,18 +84,18 @@ node('docker') {
             currentBuild.result = 'FAILURE'
         }
         finally {
-            if (!isRelease) {
+            if (!isDeliver) {
                 cleanup()
             }
         }
     }
 
-    if (isRelease) {
+    if (isDeliver) {
         stage('Deliver') {
             try {
                 withEnv(projectSettings) {
                     withDockerRegistry('nexusDeployer') { def registry ->
-                        withReleaseTag(projectDir, 'scm') { def releaseTag ->
+                        withReleaseTag('scm') { def releaseTag ->
                             def releaseImageName = "${registry}${name}:${releaseTag}"
                             tagImage buildImageName, releaseImageName
                             pushImage releaseImageName
